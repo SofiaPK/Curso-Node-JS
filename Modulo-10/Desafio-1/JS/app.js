5f35f11115dea330c54766681de58e087f8d7159
SofiaPK <melli-soo@hotmail.com>
2022-07-11 19:00:24 -0300

    agregue una carpeta

---
+++ Modulo-10/Desafio-1/JS/app.js
@@ -0,0 +1,82 @@
+//alert("hola!"); variable tipo string a traves de una ventana de alerta
+//ANOTACIONES
+
+//var edad = 23; variable numerica tipo entero
+//var total = 987.456; variable numerica tipo decimal
+//document.write("Hola estoy dentro de mi documento") permite escribir directamente en nuestro documento
+//document.write(<h1>"Hola estoy dentro de mi documento"</h1>) se pueden implementar etiquetas html
+//document.write(<h3>"Hola estoy <strong>dentro</strong> nuevamente"</h3>) se pueden implementar etiquetas html
+//console.log("estoy probando info en la consola"); 
+
+//= es un operador de asignacion, se usa para guardar el valor en una variable
+
+/*var numero = 12;
+++numero; operador de incremento se indica con el prefijo ++
+alert(numero);*/
+
+/*var numero = 15;
+--numero; operador de decremento se indica con el prefijo --
+alert(numero);*/
+
+/*los operadores de resta (-), suma (+), multiplicacion (*) y division (/)
+nos permiten hacer operaciones matematicas, por ejemplo:
+var sueldo = 9865456.98;
+var bonus = 654;
+var total = sueldo + bonus;
+alert(total)*/
+
+/*el operador matematico se puede combinar con el de asignacion
+var numero1 = 9;
+numero1 += 4; esto es numero1 = numero1 + 4
+numero1 -= 3; esto es numero1 = numero1 -3
+numero1 *= 2; esto es numero1 = numero1 *2
+numero1 /= 3; esto es numero1 = numero1 /3
+numero1 %= 5; esto es numero1 = numero1 %5  (%) se usa para calcular el resto de la division entre 2 valores enteros. Tiene la misma precedencia que el producto y el cociente*/
+
+/*el (+) permite concatenar texto:
+var sueldo = 235563;
+var bonus = 456;
+var total = sueldo + bonus;
+alert("El total del sueldo y el bonus es" + total)*/
+//document.write("El total del <strong>sueldo</strong> y el <strong>bonus</strong> es" + total)  se puede concatenar info con etiquetas HTML pero usando unicamente document.write*/
+
+/*variables booleanas (true/false)
+el operador de negacion es (!)
+var dato = 2;
+var vacio = !dato;
+alert(vacio) da resultado false*/
+
+/*l operador and (&&) obtiene un resultado donde ambos valores deben ser verdaderos, por ej
+var valorA = true;
+var valorB = false;
+resultado = valor1 && valor2; resultado = false
+
+valorA = true;
+valorB = true;
+resultado = valor1 && valor2; resultado = true*/
+
+/*el operador or (||) obtiene un resultado donde ambos valores no pueden ser verdaderos, ya que la consigna
+es que uno de ellos lo sea para obtener un resultado true, por ej
+var valorA = true;
+var valorB = false;
+resultado = valor1 || valor2; resultado = true
+
+var valorA = false;
+var valorB = false;
+resultado = valor1 || valor2; resultado = false*/
+
+/*los operadores relacionales (<) (>) permiten realizar programas complejos y comparar datos,
+el resultado sera igual a los operadores logicos true y false
+
+var numero1 = 1;
+var numero2 = 20;
+resultado = numero1 > numero2; resultado = false, mayor que 
+resultado = numero1 < numero2; resultado = true, menor que 
+
+numero1 = 10;
+numero2 = 10;
+resultado = numero1 >= numero2; resultado = true, mayor o igual que 
+resultado = numero1 <= numero2; resultado = true, menor o igual que 
+resultado = numero1 == numero2; resultado = true, igual en valor 
+resultado = numero1 != numero2; resultado = false, no es igual*/
+
