37d67a6af9ac887533197a3ccd7d244b60a75a7c
SofiaPK <melli-soo@hotmail.com>
2022-08-05 20:53:25 -0300

    desafios modulo 14

--- Modulo-14/Desafio-2/JS/codigo.js
+++ Modulo-14/Desafio-2/JS/codigo.js
@@ -1,18 +1,30 @@
 //función que reciba el array de inversiones y retorne un array agregando a cada ítem el campo “interés” con la fórmula correspondiente.
 
-var creditos = [
+/*
+ - mundo de la definicion de funcion
+ */
+function calcularInteres(listaDeInversiones){
+    for (var i = 0; i < listaDeInversiones.length; i++){
+        var interes = (listaDeInversiones[i].capital * listaDeInversiones[i].plazo * listaDeInversiones[i].tasa)/100
+        // guardo *interes* en 
+        listaDeInversiones[i].interes = interes
+    }
+
+    return listaDeInversiones;
+}
+
+/*
+ - el mundo del uso de la funcion. 
+ - aca defino variables a usar. llamo a la funcion, las veces que quiera.
+ */
+
+
+var creditosInver = [
     {nombre: 'plan001', capital: 150000, plazo: 30, tasa: 0.15},
     {nombre: 'plan002', capital: 300000, plazo: 180, tasa: 0.10},
     {nombre: 'plan003', capital: 485000, plazo: 60, tasa: 0.23},
 ];
 
-var resultado = []
 
-for (var i = 0; i < creditos.length; i++){
-    var valor = (creditos[i].capital * creditos[i].plazo * creditos[i].tasa)/100
-    var operacion = 'Al '.concat(creditos[i].nombre.concat(" le corresponde un credito de " + valor))
-
-    resultado.push(operacion);
-}
-
-console.log(resultado);
+var miInteres = calcularInteres(creditosInver)
+console.log(miInteres);
